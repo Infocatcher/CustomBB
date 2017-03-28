0a88000bbfd97963be632bcf9b03e6c237ef6249
Infocatcher <infocatcher.dev.null@gmail.com>
2017-03-28 21:29:06 +0300

    Fix script to make packed extension with .jar inside (copy-pasted and not changed in all places)

--- make_packed.bat
+++ make_packed.bat
@@ -1,6 +1,7 @@
 @echo off
 set _out=custombb-latest-packed.xpi
 set _tmpDir=%~d0\~%_out%.tmp
+set _jar=custombb.jar
 
 set _7zip="%COMMANDER_PATH%\arch\7-Zip-4.65\7z.exe"
 set _winRar="%COMMANDER_PATH%\arch\WinRAR\WinRAR.exe"
@@ -17,6 +18,12 @@ if not exist %_7zip% (
 	)
 )
 
+if exist %_tmpDir% (
+	echo Error: found %_tmpDir% directory, please remove/rename it first!
+	pause
+	exit /b
+)
+
 cd /d "%~dp0"
 
 set _files=install.rdf *.manifest *.js *.jsm *.xul *.xml *.html license* *.png defaults modules components locale chrome idl
@@ -26,7 +33,7 @@ xcopy /e * %_tmpDir%
 
 del /q %_tmpDir%\*.xpi
 del /q %_tmpDir%\*.bat
-del /q %_tmpDir%\chrome\handyclicks.jar
+del /q %_tmpDir%\chrome\%_jar%
 move /y %_tmpDir%\chrome_.manifest %_tmpDir%\chrome.manifest
 
 :: Copy last modified time of directories
@@ -34,13 +41,14 @@ xcopy /e /t /y * %_tmpDir%
 
 if exist %_7zip% (
 	echo =^> %_7zip%
-	%_7zip% a -tzip -mx0 -- %_tmpDir%\chrome\handyclicks.jar %_tmpDir%\chrome\content %_tmpDir%\chrome\locale
+	%_7zip% a -tzip -mx0 -- %_tmpDir%\chrome\%_jar% %_tmpDir%\chrome\content %_tmpDir%\chrome\skin %_tmpDir%\chrome\locale
 ) else (
 	echo =^> %_winRar%
-	%_winRar% a -afzip -m0 -r -- %_tmpDir%\chrome\handyclicks.jar %_tmpDir%\chrome\content %_tmpDir%\chrome\locale
+	%_winRar% a -afzip -m0 -r -- %_tmpDir%\chrome\%_jar% %_tmpDir%\chrome\content %_tmpDir%\chrome\skin %_tmpDir%\chrome\locale
 )
 
 rd /s /q %_tmpDir%\chrome\locale
+rd /s /q %_tmpDir%\chrome\skin
 rd /s /q %_tmpDir%\chrome\content
 
 pushd %_tmpDir%
